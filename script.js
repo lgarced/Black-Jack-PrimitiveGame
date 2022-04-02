ad3bf5bf33c3a11af55ad3f6cab0d16870b5fb7d
Liberato Garced <93692943+lgarced@users.noreply.github.com>
2022-04-02 15:50:52 -0700

    More changes

---
+++ script.js
@@ -0,0 +1,38 @@
+let firstCard = 10
+let secondCard = 4
+let cards =[firstCard , secondCard]
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let cardsEl = document.getElementById("cards-el")
+
+let sumEl = document.querySelector(".sum-el")
+
+function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+function addCard() {
+    console.log("Drawing a new card from the deck!")
+   let card = 7
+    
+    sum += card
+  
+    renderGame()
+}
